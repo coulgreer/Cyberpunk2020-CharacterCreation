1039b8fd5a727c3cbee7b9b5f1ff531ae5f851c7
Coul Greer <coulgreer1@hotmail.com>
2018-01-20 02:41:14 -0500

    Deleted the unneeded code.
    This includes code created by WindowBuilder and the old view page that
    was manually created

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,17 +1,26 @@
 package core;
 
+import java.awt.EventQueue;
+
 import controller.CharacterCreationController;
 import model.Character;
-import view.CharacterCreationPage;
+import view.CharacterCreationView;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Character character = new Character();
-		CharacterCreationPage page = new CharacterCreationPage();
-		
-		page.setVisible(true);
-		page.initializeGUI();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Character character = new Character();
+					CharacterCreationView frame = new CharacterCreationView();
+					CharacterCreationController controller = new CharacterCreationController(character, frame);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
--- src/view/CharacterCreationView.java
+++ src/view/CharacterCreationView.java
@@ -94,24 +94,7 @@ public class CharacterCreationView extends JFrame {
 	private JPanel intelligenceSkillPanel;
 	private List<JPanel> stunProgressPanels = new ArrayList<JPanel>();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Character character = new Character();
-					CharacterCreationView frame = new CharacterCreationView();
-					CharacterCreationController controller = new CharacterCreationController(character, frame);
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the frame.
 	 */
